52933531ed867329e3531819476e645d3f8a1337
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-03-21 20:00:09 +0000

    Implement WeatherDataset Also force rename the datetime_column in EnergyDataset to 'DATE-TIME'. This is required for merging weather and energy datasets later on

--- dataset.py
+++ dataset.py
@@ -8,28 +8,51 @@ class Dataset:
     def __init__(self, path: str):
         self.path = path
 
-    def clean(self) -> DataFrame:
+    def clean(self) -> None:
         pass
 
     def visualise(self, figures=str | list[str]) -> None:
         pass
 
 class WeatherDataset(Dataset):
-    pass
+    def __init__(self, path: str) -> None:
+        super().__init__(path)
+        self.dataset = pd.read_csv(self.path, skiprows=16)
+
+    def clean(self) -> None:
+        self.dataset.rename(columns={
+            'YEAR': 'Year',
+            'MO': 'Month',
+            'DY': 'Day',
+            'HR': 'Hour',
+            'ALLSKY_SFC_SW_DWN': 'Downward Irradiance',
+            'ALLSKY_SFC_UV_INDEX': 'UV Index',
+            'ALLSKY_KT': 'Clearness Index',
+            'T2M': 'Temperature',
+            'QV2M': 'Humidity',
+            'PS': 'Pressure',
+            'WS10M': 'Wind Speed',
+            'WD10M': 'Wind Direction'
+        }, inplace=True)
+        self.dataset['DATE-TIME'] = pd.to_datetime(self.dataset[['Year', 'Month', 'Day', 'Hour']])
+        self.dataset = self.dataset.drop(columns=['Year', 'Month', 'Day', 'Hour'])
+
+    def visualise(self, figures=str | list[str]) -> None:
+        print(self.dataset.head())
+
 
 class EnergyDataset(Dataset):
     def __init__(self, path: str, datetime_column: str='DATE-TIME') -> None:
         super().__init__(path)
         self.dataset = pd.read_excel(path)
-
-        self.datetime_column = datetime_column
+        self.dataset.rename(columns={datetime_column: 'DATE-TIME'}, inplace=True)
 
     def clean(self) -> None:
         # Convert DateTime values to Pandas DataTime values
-        self.dataset[self.datetime_column] = pd.to_datetime(self.dataset[self.datetime_column])
+        self.dataset['DATE-TIME'] = pd.to_datetime(self.dataset['DATE-TIME'])
 
         # Extract date to separate column
-        self.dataset['Date'] = self.dataset[self.datetime_column].dt.date
+        self.dataset['Date'] = self.dataset['DATE-TIME'].dt.date
 
         # Calculate daily average and merge back into table
         daily_avg = self.dataset.groupby('Date')['MW'].mean().reset_index()
@@ -44,3 +67,16 @@ class EnergyDataset(Dataset):
         daily_avg_plot.set_title('Average Daily PV load in 2021')
 
         plt.show()
+
+if __name__ == '__main__':
+    energy_demand: EnergyDataset = EnergyDataset('data/Sakakah 2021 Demand dataset.xlsx')
+    energy_demand.clean()
+
+    energy_supply: EnergyDataset = EnergyDataset('data/Sakakah 2021 PV Supply dataset.xlsx', datetime_column='Date & Time')
+    energy_supply.clean()
+
+    weather_demand: WeatherDataset = WeatherDataset('data/Sakakah 2021 weather dataset Demand.csv')
+    weather_demand.clean()
+
+    weather_supply: WeatherDataset = WeatherDataset('data/Sakakah 2021 weather dataset.csv')
+    weather_supply.clean()
--- ui/datasets.py
+++ ui/datasets.py
@@ -2,7 +2,7 @@ from typing import Callable
 
 import survey
 
-from dataset import EnergyDataset
+from dataset import EnergyDataset, WeatherDataset
 
 
 def _visualise_energy_demand() -> None:
@@ -10,8 +10,18 @@ def _visualise_energy_demand() -> None:
     dataset.clean()
     dataset.visualise('all')
 
+def _visualise_weather_demand() -> None:
+    dataset: WeatherDataset = WeatherDataset('data/Sakakah 2021 weather dataset Demand.csv')
+    dataset.clean()
+    dataset.visualise('all')
+
 def _visualise_energy_supply() -> None:
     dataset: EnergyDataset = EnergyDataset('data/Sakakah 2021 PV Supply dataset.xlsx', datetime_column='Date & Time')
+    dataset.clean()
+    dataset.visualise('all')
+
+def _visualise_weather_supply() -> None:
+    dataset: WeatherDataset = WeatherDataset('data/Sakakah 2021 weather dataset.xlsx')
     dataset.clean()
     dataset.visualise('all')
 
@@ -30,8 +40,14 @@ def display_menu(on_exit: Callable[[], None]=lambda: ()) -> None:
         case 0:
             _visualise_energy_demand()
             display_menu(on_exit=on_exit)
+        case 1:
+            _visualise_weather_demand()
+            display_menu(on_exit=on_exit)
         case 2:
             _visualise_energy_supply()
+            display_menu(on_exit=on_exit)
+        case 1:
+            _visualise_weather_supply()
             display_menu(on_exit=on_exit)
         case _:
             on_exit()
