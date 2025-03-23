39bf0b490d94f8287a454ef91bb6453dd0da6cf1
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-03-23 18:29:13 +0000

    Create basic decision tree class Also stub out a runner for prediction models

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,12 @@
 from typing import Any
 
+class PredictionModelRunner:
+    def __init__(self, config: dict) -> None:
+        self.config: dict = config
+
+    def run_models(self) -> None:
+        pass
+
 
 class PredictionModel:
     def __init__(self, name:str, config: dict[str, Any]) -> None:
@@ -7,8 +14,8 @@ class PredictionModel:
             config['Model'] = name
             config_hash_code = hash(config)
 
-    def train(self) -> None:
+    def train(self, x: Any, y: Any, validation_data: tuple = None) -> None:
         pass
 
-    def predict(self) -> None:
+    def predict(self, x: Any) -> Any:
         pass
--- models/prediction/decision_tree.py
+++ models/prediction/decision_tree.py
@@ -1,8 +1,17 @@
 from typing import Any
 
+from sklearn.tree import DecisionTreeClassifier
+
 from models import PredictionModel
 
 
 class DecisionTreeModel(PredictionModel):
     def __init__(self, config: dict[str, Any]):
         super().__init__('Decision Tree', config)
+        self.model = DecisionTreeClassifier()
+
+    def train(self, x: Any, y: Any, validation_data: tuple = None) -> None:
+        self.model = DecisionTreeClassifier().fit(x, y)
+
+    def predict(self, x: Any) -> Any:
+        return self.model.predict(x)
