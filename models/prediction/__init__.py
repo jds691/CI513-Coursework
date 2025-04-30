489cadd4f6ccda762ad7638711b8f75ae4648356
Neo Salmon <52429194+jds691@users.noreply.github.com>
2025-04-30 15:54:08 +0100

    Implement BiLSTM functionality The runner can now modify the inputs required by the model if it needs 3D inputs. This is very closely modeled off of the existing work
    This model is not prone to overfitting it seems

--- models/prediction/__init__.py
+++ models/prediction/__init__.py
@@ -1,12 +1,17 @@
 from typing import Any
 
+from keras.src.layers import Bidirectional
 from sklearn.ensemble import ExtraTreesRegressor, RandomForestRegressor
 from sklearn.tree import DecisionTreeRegressor
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, LSTM
 
 from models.config import ConfigOption, PredictionModelName
 
 
 class PredictionModel:
+    require_3d_input = False
+
     def __init__(self, name:str, config: dict[str, Any]) -> None:
         if not config[ConfigOption.DISABLE_CACHE]:
             config['Model'] = name
@@ -57,3 +62,14 @@ class RandomForestModel(PredictionModel):
 class BiLSTMModel(PredictionModel):
     def __init__(self, config: dict[str, Any]):
         super().__init__(PredictionModelName.BILSTM, config)
+        self.require_3d_input = True
+        self.model = Sequential()
+        self.model.add(Bidirectional(LSTM(50, activation='relu')))
+        self.model.add(Dense(1))  # output layer
+        self.model.compile(optimizer='adam', loss='mse')
+
+    def train(self, x: Any, y: Any, validation_data: tuple = None) -> None:
+        self.model.fit(x, y, epochs=20, batch_size=32, validation_data=validation_data, verbose=1)
+
+    def predict(self, x: Any) -> Any:
+        return self.model.predict(x)
